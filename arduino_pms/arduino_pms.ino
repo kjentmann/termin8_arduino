99a18ec2d80ccf6514e8c1ca3c37dc753800ffee
Your Name <you@example.com>
2017-04-20 14:10:53 +0200

    improve code and stability for BT comunication

--- arduino_pms/arduino_pms.ino
+++ arduino_pms/arduino_pms.ino
@@ -6,14 +6,10 @@
 //------------------PMS-------------------//
 //----------------------------------------//
 
-
-
 #include <dht.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
-
-
 
 #define DHT11_PIN 2
 byte humiditySensorApin = A0;
@@ -31,6 +27,7 @@ int currentAirTemp;
 int currentAirHumid;
 int currentSoilHumid;
  
+# define MAX_WATER_DURATION 60
 
 void setup() {
 
@@ -78,50 +75,75 @@ void waterPlant(double duration){ //sec
 
 String data ="";
 void bluetoothSync(){
-   while (BT.available())
-    {
+ 
+   while (BT.available()){
+     
         char character = BT.read(); // Receive a single character from the software serial port
-        data.concat(character); // Add the received character to the receive buffer
+           int charr= character;
+           BT.println(charr); //RAW data control
+          data.concat(character); // Add the received character to the receive buffer
         if (character == '\n')
         {
-          
+         int lenght = data.length()-3;  
   
-              
-        if (data[0]=='1'){
-            digitalWrite(waterPumpRelayPin, LOW);
+        if (lenght <=2){  
+        switch (data[0]){
+        
+        case '1':  
+       
+            //digitalWrite(waterPumpRelayPin, LOW);  // Disable for safety reasons
             BT.println("Water pump on");
-        }
-        else if (data[0]=='2'){
-          digitalWrite(waterPumpRelayPin, HIGH);
+        //}
+        break;
+        case '2':
+        
+          //digitalWrite(waterPumpRelayPin, HIGH);  // Disable for safety reasons
           BT.println("Water pump off");
-        }
-        else if (data[0]=='3'){
-          int duration = data[1]-48; //whooops max 9 sec
-          BT.println("Watering for ");
+        break;
+        
+        case '?': 
+          BT.println("Send '1' to turn water pump on");
+          BT.println("Send '2' to turn water pump off");
+        
+        break;
+        
+        case '3':
+          int duration; 
+          char buffer[2];
+          BT.println("lenght of data:");
+          BT.println(lenght);
+          if (lenght ==1){  
+            duration = data[1]-48;
+            BT.println("a quick one!");  
+          }
+          else if (lenght ==2){
+            buffer[0]=data[1];
+            buffer[1]=data[2];
+            buffer[2]= '\0';
+            duration = atoi(buffer);
+            BT.println("wow, thirsty one!");
+            BT.println(duration);
+          }
+                    
+        if (duration>0 && duration < MAX_WATER_DURATION){
+          BT.println("Watering for num sec:");
           BT.println(duration);
-          if (duration>0){
-            waterPlant(duration);
-          }
-          else{
-            BT.println("Error! non valid duration");  
-          }
+          waterPlant(duration);
+        }
+        else{
+          BT.println("Error! non valid duration");  
+        }
+      break; 
       }
-    else if (data[0]=='?')
-    {
-      BT.println("Send '1' to turn water pump on");
-      BT.println("Send '2' to turn water pump off");
     }
     else{
-         BT.println("Unknown command. Received: ");
-         BT.println(data);
+      BT.println("Unknown command. Received: ");
+      BT.println(data);
     }
-
-        
-            // Clear buffer
-            data = "";
-        }
-    }
-  
+    data = "";// Clear buffer
+    BT.println("\nTermin8\n");
+  }
+ }
 }
 
 
@@ -135,16 +157,12 @@ void loop()
   bluetoothSync();
 }
 
-
+  //Serial prints if no lcd in the hood.
+  
   //Serial.print("Air temperature = ");
   //Serial.println(currentAirTemp);
   //Serial.print("Air humidity = ");
   //Serial.println(currentAirHumid);
-   // Serial.print("Humidity Level (0-1023): ");
+  // Serial.print("Humidity Level (0-1023): ");
   // 1023 from specs, while highest walue from MADSOFT labs is measured to 750
   //Serial.println(read_humidity_sensor());
-
- 
-   
-
-

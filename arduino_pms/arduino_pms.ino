57c13de7a5b5cb63984e551a29133884d365d13e
Your Name <you@example.com>
2017-04-26 12:15:07 +0200

    send sensor data on BT. Not possible to turn on pump whitout time limit. Clean code.

--- arduino_pms/arduino_pms.ino
+++ arduino_pms/arduino_pms.ino
@@ -61,10 +61,7 @@ void updateLCD(){
   lcd.print("AirT:" + String(currentAirTemp));
   lcd.print(" AirH:" + String(currentAirHumid));
   lcd.setCursor(0,1);
-  lcd.print("SoilHumidity:" + String(currentSoilHumid));
-  //lcd.print(String(currentAirTemp));
-
-  
+  lcd.print("SoilHumidity:" + String(currentSoilHumid)); 
 }
 
 void waterPlant(double duration){ //sec
@@ -74,39 +71,38 @@ void waterPlant(double duration){ //sec
 }
 
 String data ="";
-void bluetoothSync(){
- 
-   while (BT.available()){
-     
-        char character = BT.read(); // Receive a single character from the software serial port
-           int charr= character;
-           BT.println(charr); //RAW data control
-          data.concat(character); // Add the received character to the receive buffer
-        if (character == '\n')
-        {
-         int lenght = data.length()-3;  
-  
-        if (lenght <=2){  
-        switch (data[0]){
-        
-        case '1':  
-       
-            //digitalWrite(waterPumpRelayPin, LOW);  // Disable for safety reasons
-            BT.println("Water pump on");
-        //}
+void bluetoothReceive(){
+ while (BT.available()){
+    char character = BT.read(); // Receive a single character from the software serial port
+    int charr= character;
+    BT.println(charr); //RAW data control
+    data.concat(character); // Add the received character to the receive buffer
+    if (character == '\n'){
+     int lenght = data.length()-3;  
+     if (lenght <=2){  
+     switch (data[0]){
+      
+      case '1':  
+          //digitalWrite(waterPumpRelayPin, LOW);  //  Water pump ON: isabled for safety reasons
+          BT.println("No aviable action");
+          break;
+      case '2':
+        digitalWrite(waterPumpRelayPin, HIGH);
+        BT.println("Water pump OFF");
         break;
-        case '2':
-        
-          //digitalWrite(waterPumpRelayPin, HIGH);  // Disable for safety reasons
-          BT.println("Water pump off");
+      
+      case '?':
+        BT.println("--------Termin-8 Projects-----.---");
+        BT.println("----------------------------------");
+        BT.println("-Hardcore plant management system-");
+        BT.println("----------------------------------");
+        BT.println("---------------PMS----------------");
+        BT.println("----------------------------------");
+        BT.println("Info:");
+        BT.println("PMS sends sensor data at 0.5 Hz"); 
+        BT.println("Send '3' + 'int(x)' to water for x sec");
         break;
-        
-        case '?': 
-          BT.println("Send '1' to turn water pump on");
-          BT.println("Send '2' to turn water pump off");
-        
-        break;
-        
+      
         case '3':
           int duration; 
           char buffer[2];
@@ -125,44 +121,46 @@ void bluetoothSync(){
             BT.println(duration);
           }
                     
-        if (duration>0 && duration < MAX_WATER_DURATION){
-          BT.println("Watering for num sec:");
-          BT.println(duration);
-          waterPlant(duration);
-        }
-        else{
-          BT.println("Error! non valid duration");  
-        }
-      break; 
+          if (duration>0 && duration < MAX_WATER_DURATION){
+            BT.println("Watering for num sec:");
+            BT.println(duration);
+            waterPlant(duration);
+          }
+          else{
+            BT.println("Error! non valid duration");  
+          }
+          break; 
       }
-    }
-    else{
-      BT.println("Unknown command. Received: ");
-      BT.println(data);
-    }
-    data = "";// Clear buffer
-    BT.println("\nTermin8\n");
+  }
+  else{
+    BT.println("Unknown command. Received: ");
+    BT.println(data);
+  }
+  data = "";// Clear buffer
+  BT.println("\nTermin8\n");
   }
  }
 }
 
-
+void bluetoothSend(){
+ String airTemp ="0";
+ String airHumid="1";
+ String soilHumid="2";
+ airTemp.concat(String(currentAirTemp));
+ airHumid.concat(String(currentAirHumid));
+ soilHumid.concat(String(currentSoilHumid));
+ BT.println(airTemp);
+ BT.println(airHumid);
+ BT.println(soilHumid);  
+}
+  
 void loop()
 {
   int chk = DHT.read11(DHT11_PIN);
   currentAirTemp = DHT.temperature;
   currentAirHumid = DHT.humidity;
-  currentSoilHumid =read_humidity_sensor();
+  currentSoilHumid =read_humidity_sensor(); //includes 500 ms delay
   updateLCD();
-  bluetoothSync();
+  bluetoothReceive();
+  bluetoothSend(); 
 }
-
-  //Serial prints if no lcd in the hood.
-  
-  //Serial.print("Air temperature = ");
-  //Serial.println(currentAirTemp);
-  //Serial.print("Air humidity = ");
-  //Serial.println(currentAirHumid);
-  // Serial.print("Humidity Level (0-1023): ");
-  // 1023 from specs, while highest walue from MADSOFT labs is measured to 750
-  //Serial.println(read_humidity_sensor());
